3fe1a4b0cb153d23eef8be5e2dcd4f411eb6ba87
riyasingh18 <riyasingh9618@gmail.com>
2023-06-29 14:30:46 +0530

    add change in data service

--- first/src/app/services/data.service.ts
+++ first/src/app/services/data.service.ts
@@ -2,15 +2,20 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { todos } from '../model/todos';
+import { posts } from '../model/posts';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  public url = 'https://jsonplaceholder.typicode.com/todos';
+  public url = 'https://jsonplaceholder.typicode.com';
+
   constructor(private http: HttpClient) {}
 
   getTodos(): Observable<todos[]> {
-    return this.http.get<todos[]>(this.url);
+    return this.http.get<todos[]>(this.url + '/todos');
+  }
+  getPosts(): Observable<posts[]> {
+    return this.http.get<posts[]>(this.url + '/posts');
   }
 }

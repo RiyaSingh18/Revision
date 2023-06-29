e3acf183270295cb68e7699fb34db007b76d2b92
riyasingh18 <riyasingh9618@gmail.com>
2023-06-29 14:40:20 +0530

    added checkbox in course component

--- first/src/app/components/dashboard/courses/courses.component.ts
+++ first/src/app/components/dashboard/courses/courses.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  styleUrls: ['./courses.component.css'],
 })
 export class CoursesComponent {
+  userForm!: FormGroup;
+  public postsdata: any;
+  multicheck: boolean = false;
+  checkedItems: any[] = [];
 
+  constructor(private fb: FormBuilder, private data: DataService) {
+    this.userForm = this.fb.group({
+      id: new FormControl(''),
+      username: new FormControl(''),
+      email: new FormControl(''),
+      city: new FormControl(''),
+    });
+  }
+  ngOnInit() {
+    this.data.getPosts().subscribe((posres) => {
+      this.postsdata = posres;
+      console.log(this.postsdata);
+    });
+  }
+
+  savedata(data: any) {
+    console.log(data);
+  }
+  checkeditems(check: any) {
+    //this.multicheck = !this.multicheck;
+    if (this.multicheck) {
+      console.log('checked' + check);
+    } else {
+      console.log('not checked' + check);
+    }
+  }
 }
